13860e5b1c35acd4bf5e9c29589430ab6184f399
integ3r <omgtehnoes@gmail.com>
2018-02-04 18:00:30 +0100

    Initial centralization of controls / options

---
+++ Gronank's stuff/Assets/Options/Controls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Controls
+{
+    class Controls 
+    {
+        // Camera
+        public static KeyCode NORTH = KeyCode.W;
+        public static KeyCode SOUTH = KeyCode.S;
+        public static KeyCode WEST = KeyCode.A;
+        public static KeyCode EAST = KeyCode.D;
+
+        // Unit hotkeys
+        public static KeyCode FIREPOSITION = KeyCode.T;
+        public static KeyCode REVERSE = KeyCode.R;
+        public static KeyCode STOP = KeyCode.H;
+        public static KeyCode HOLDFIRE = KeyCode.Z;
+        public static KeyCode UNLOAD = KeyCode.U;
+        public static KeyCode LOAD_UP = KeyCode.Y;
+
+    }
+}
---
+++ Gronank's stuff/Assets/Options/Options.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Options  {
+    public static bool StickySelection = true;
+    public static int scrollspeed = 1; //Todo implement
+    
+
+}
